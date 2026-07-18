121b221bf35d7fd4388dd03d231e9c38cb09280a
Sn1kt <155704629+W1nningSmile@users.noreply.github.com>
2026-07-18 01:22:31 -0400

    initial setup changed to be more robust
    Initial setup now only checks for missing folders and creates the missing ones. I might need to add a check for important files and gui elements later to make it more robust.

--- config.py
+++ config.py
@@ -1,28 +1,15 @@
 from pathlib import Path
+import sys
 
-BASE_DIR = Path(__file__).parent
+if getattr(sys, "frozen", False):
+    BASE_DIR = Path(sys.executable).parent
+else:
+    BASE_DIR = Path(__file__).parent
 
 IMG_RESS_DIR = BASE_DIR / "image_ressources"
-
-SONG_DIR = BASE_DIR / "songs/Album"
-
-UI_DIR = BASE_DIR / "ui"
-
-REQUIRED_FILES = [
-    #image_ressources
-    IMG_RESS_DIR / "default_cover.jpg", 
-    IMG_RESS_DIR / "magnifying-glass-icon.png",
-    IMG_RESS_DIR / "Speaker_Icon.png",
-    IMG_RESS_DIR / "template.png",
-
-    #ui
-    UI_DIR / "album_widget_template.py",
-    UI_DIR / "create_album_ui.py",
-    UI_DIR / "main_page_ui.py"
-]
+SONG_DIR = BASE_DIR / "songs" / "Album"
 
 REQUIRED_FOLDERS = [
     IMG_RESS_DIR,
     SONG_DIR,
-    UI_DIR
 ]
